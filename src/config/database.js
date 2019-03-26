038d2849a3b5e01fe3bdcb20547a8e2d986bbe21
Lucas Alves <lucas.alves@udf.org.br>
2019-03-26 14:12:11 -0300

    Primeiro commit node agendai

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '172.17.0.3',
+  username: 'lucas',
+  password: '0rZFiwRE',
+  database: 'agendai',
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
